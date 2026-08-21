ed7dcedcf4522c2aab05c02d558f1f0a633e1e2e
Nashly Alfaro Berrocal <nashly.alfaro.berrocal.@est.una.ac.cr>
2026-08-21 15:53:11 -0600

    funcion A.2a y A.2b

--- lab2_progra/SolutionA.cpp
+++ lab2_progra/SolutionA.cpp
@@ -1,13 +1,14 @@
 
 #include <iostream>
 
+//A.1a
     int countGreaterThan(int* array, int size, int limit){
-        int count = 0;
+        int count =0;
 
-        for (int i = 0; i < size; i++)
-        {
-            if (array[i] > limit)
-            {
+        for (int i =0;i < size; i++) {
+
+            if (array[i] > limit){
+
                 count++;
             }
         }
@@ -15,17 +16,48 @@
         return count;
     }
 
+//A.1b
+    double average(int* array, int size){
+        int total =0;
 
-    double average(int* array, int size)
-    {
-        int total = 0;
+        for (int i =0; i< size; i++){
 
-        for (int i = 0; i < size; i++)
-        {
             total += array[i];
         }
 
         return static_cast<double>(total) / size;
     }
 
+
+//A.2a
+    void sumByRow(int matrix[3][4], int result[3]) {
+        for (int i= 0; i< 3; i++)
+        {
+            int total =0;
+
+            for (int j= 0; j < 4; j++){
+
+                total += matrix[i][j];
+            }
+
+            result[i]= total;
+        }
+    }
+
+//A.2b
+    int countValue(int matrix[3][4], int value){
+        int count =0;
+
+        for (int i =0; i< 3; i++){
+
+            for (int j = 0; j < 4; j++){
+
+                if (matrix[i][j]== value){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 
--- lab2_progra/SolutionA.h
+++ lab2_progra/SolutionA.h
@@ -1,4 +1,13 @@
 #pragma once
 
+//A.1a 
 int countGreaterThan(int* array, int size, int limit);
+
+//A.1b
 double average(int* array, int size);
+
+//A.2a
+void sumByRow(int matrix[3][4], int result[3]);
+
+//A.2b
+int countValue(int matrix[3][4], int value);
