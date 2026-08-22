35fd3fa9c6fc510e0a8557244955f77289cd5310
Nashly Alfaro Berrocal <nashly.alfaro.berrocal.@est.una.ac.cr>
2026-08-21 19:21:09 -0600

    implementacion de las  funciones en l .h y primer funcion del cpp

--- lab2_progra/SolutionB.cpp
+++ lab2_progra/SolutionB.cpp
@@ -1,1 +1,31 @@
-#include "solucionB.h"
+#include "SolutionB.h"
+#include <iostream>
+
+//B.1
+void addProduct(std::string*& productNames, double*& productPrices, int*& productStock, int& productCount, std::string name, double price,int qty){
+
+    int newSize= productCount + 1;
+
+    std::string* newNames = new std::string[newSize];
+    double* newPrices = new double[newSize];
+    int* newStock =new int[newSize];
+
+    for (int i =0; i < productCount; i++){
+        newNames[i]= productNames[i];
+        newPrices[i]= productPrices[i];
+        newStock[i] = productStock[i];
+    }
+
+    newNames[productCount] = name;
+    newPrices[productCount] = price;
+    newStock[productCount] = qty;
+
+    delete[] productNames;
+    delete[] productPrices;
+    delete[] productStock;
+
+    productNames= newNames;
+    productPrices = newPrices;
+    productStock= newStock;
+    productCount++;
+}
--- lab2_progra/SolutionB.h
+++ lab2_progra/SolutionB.h
@@ -1,5 +1,29 @@
-#pragma once
-class solucionB
-{
-};
+#ifndef SOLUTIONB_H
+#define SOLUTIONB_H
 
+#include <string>
+
+//B.1
+void addProduct(std::string*& productNames, double*& productPrices, int*& productStock, int& productCount, std::string name, double price, int qty);
+
+//B.2
+int findProduct(std::string* productNames, int productCount, std::string name);
+
+
+//B.3
+void updateStock(int* productStock, int index, int newQty);
+
+//B.4
+void priceMinMax(double* productPrices, int productCount, double* minPrice,double* maxPrice);
+
+//B.5
+
+
+
+//B.6
+void showInventory(std::string* productNames, double* productPrices, int* productStock, int productCount);
+
+//B.7
+void freeAll(std::string* productNames, double* productPrices, int* productStock);
+
+#endif
